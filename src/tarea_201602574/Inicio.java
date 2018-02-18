6b458d00c7a8ed051424479fde09a3746f479aec
hp <hp@Familia-Gonzalez-Reyes>
2018-02-17 18:46:15 -0600

    Tarea3 inicializada

---
+++ src/tarea_201602574/Inicio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_201602574;
+
+/**
+ *
+ * @author hp
+ */
+public class Inicio {
+    public Inicio(){
+        
+    }
+}
